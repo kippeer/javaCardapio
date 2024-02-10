76f18f2e05869b95f090dbb6a5b76a06144a3a7b
Fabio <fabiot92@gmail.com>
2024-02-10 18:15:52 -0300

    get cardapio

--- backend-cardapio-digital-main/src/main/java/com/example/cardapio/controller/FoodController.java
+++ backend-cardapio-digital-main/src/main/java/com/example/cardapio/controller/FoodController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("food")
+@RequestMapping("cardapio")
 public class FoodController {
 
     @Autowired
