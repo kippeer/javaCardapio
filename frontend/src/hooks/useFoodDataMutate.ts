804f21191bfe851027776caba767d4a44e4fdf00
Fabio <fabiot92@gmail.com>
2024-02-18 13:18:19 -0300

    att
    att

--- frontend/src/hooks/useFoodDataMutate.ts
+++ frontend/src/hooks/useFoodDataMutate.ts
@@ -2,7 +2,7 @@ import { useMutation, useQueryClient } from "@tanstack/react-query";
 import axios, { AxiosPromise } from "axios"
 import { FoodData } from '../interface/FoodData';
 
-const API_URL = ' http://food-env.eba-vpsi9hzm.sa-east-1.elasticbeanstalk.com/';
+const API_URL = ' http://localhost:8080'
 
 const postData = async (data: FoodData): AxiosPromise<any> => {
     const response = axios.post(API_URL + '/food', data);
